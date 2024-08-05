ab12e68178305442a66cc2f012c87fce7727ac3c
jin1125 <shogo.jinta@gmail.com>
2024-08-06 00:19:10 +0900

    feat: データがない場合の表示を実装

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,20 +9,24 @@
 </svelte:head>
 
 <div class="flex flex-col items-center mt-6 sm:mt-24 space-y-16">
-  <img
-    class="rounded-full w-72"
-    src={data.about[0]?.image?.src}
-    alt="プロフィール画像"
-  />
-  <div class="space-y-6">
-    <p>
-      居住地 / {data.about[0]?.address}
-    </p>
-    <p>
-      生年 / {data.about[0]?.birth}
-    </p>
-    <p>
-      趣味 / {data.about[0]?.hobby}
-    </p>
-  </div>
+  {#if data.about && data.about.length > 0}
+    <img
+      class="rounded-full w-72"
+      src={data.about[0]?.image?.src}
+      alt="プロフィール画像"
+    />
+    <div class="space-y-6">
+      <p>
+        居住地 / {data.about[0]?.address}
+      </p>
+      <p>
+        生年 / {data.about[0]?.birth}
+      </p>
+      <p>
+        趣味 / {data.about[0]?.hobby}
+      </p>
+    </div>
+  {:else}
+    <p>No data available</p>
+  {/if}
 </div>
