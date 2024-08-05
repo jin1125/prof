27d0ed897bd7133c5def196a4ba6b5e07f874e6d
jin1125 <shogo.jinta@gmail.com>
2024-08-05 23:39:07 +0900

    feat: aboutページを実装

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,26 @@
+<script lang="ts">
+  export let data;
+</script>
+
 <svelte:head>
   <title>About - Jin's Profile</title>
 </svelte:head>
 
-<h1>About</h1>
+<div class="flex flex-col items-center mt-6 sm:mt-24 space-y-16">
+  <img
+    class="rounded-full w-72"
+    src={data.about[0].image.src}
+    alt="プロフィール画像"
+  />
+  <div class="space-y-6">
+    <p>
+      居住地 / {data.about[0].address}
+    </p>
+    <p>
+      生年 / {data.about[0].birth}
+    </p>
+    <p>
+      趣味 / {data.about[0].hobby}
+    </p>
+  </div>
+</div>
