e012caee8744428c269974a2da32e0931a9ca664
jin1125 <shogo.jinta@gmail.com>
2024-08-06 22:40:49 +0900

    feat: injectSpeedInsightsを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import Header from "$lib/Header.svelte";
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
   import "../app.css";
+
+  injectSpeedInsights();
 </script>
 
 <Header />
